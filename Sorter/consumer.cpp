bd62ac1cd6da92f0715793c6221d9d7ba807b912
Unknown <omegazeron@yahoo.com>
2017-06-03 15:25:45 -0700

    Unsorted info

--- Sorter/consumer.cpp
+++ Sorter/consumer.cpp
@@ -19,10 +19,26 @@ void Consumer::pour()
 {
     QMutex_ptr->lock(); // lock the thread
 
-    std::sort(QList_ptr->begin(), QList_ptr->end());
-
     QString QS;
     int counter = 0;
+    QS += "Unsorted contents\n";
+    for (int i = 0; i < QList_ptr->size(); ++i)
+    {
+        counter++;
+        QS += "#";
+        if(counter < 10)
+        {
+            QS += "0"; // formatting for single digit numbers (e.g. 01, 02, 03, etc.)
+        }
+        QS += QString::number(counter) += ": ";
+        QS += QString::number(QList_ptr->at(i));
+        QS += "\n" ;
+    }
+    QS += "\n";
+
+    std::sort(QList_ptr->begin(), QList_ptr->end());
+    counter = 0;
+    QS += "Sorted contents\n";
     while(!QList_ptr->isEmpty())
     {
         counter++;
